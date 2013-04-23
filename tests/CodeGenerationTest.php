91ae5e03798f792b8580b292d70d09749f027259
Dan Cryer <dan@block8.co.uk>
2013-04-23 03:47:15 +0100

    Adding code generation tests so that they start failing.

---
+++ tests/CodeGenerationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('b8/Registry.php');
+require_once('b8/Database.php');
+require_once('b8/Database/Map.php');
+require_once('b8/Database/CodeGenerator.php');
+
+class CodeGenerationTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/DatabaseGenerationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('b8/Registry.php');
+require_once('b8/Database.php');
+require_once('b8/Database/Map.php');
+require_once('b8/Database/Generator.php');
+
+class DatabaseGenerationTest extends \PHPUnit_Framework_TestCase
+{
+
+}
