ae1c75787bc170cc350e611c3784e0d9e135e78d
Dan Cryer <dan@block8.co.uk>
2014-06-04 11:38:53 +0100

    Fixing TabPanel

--- b8/Form/FieldSet.php
+++ b8/Form/FieldSet.php
@@ -102,6 +102,7 @@ class FieldSet extends Element
             $rendered[] = $child->render();
         }
 
+        $view->childFields = $this->children;
         $view->children = $rendered;
     }
 
